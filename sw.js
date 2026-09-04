a0ad64cfd6e0cf8d7b165ed521c4d6adfcb7e5a5
kiossoufarid-hash <kiossoufarid@gmail.com>
2026-09-04 04:01:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const CACHE_NAME = "musicplayer-shell-v2";
+const APP_SHELL = [
+	"./",
+	"./index.html",
+	"./css/style.css",
+	"./js/app.js",
+	"./js/lucide.min.js",
+	"./manifest.json",
+	"./assets/icons/apple-touch-icon.png",
+	"./assets/icons/favicon-96x96.png",
+	"./assets/icons/favicon.ico",
+	"./assets/icons/favicon.svg",
+	"./assets/icons/icon-192.png",
+	"./assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(cache => cache.addAll(APP_SHELL))
+			.then(() => self.skipWaiting())
+	);
+});
+
+self.addEventListener("activate", event => {
+	event.waitUntil(
+		caches.keys()
+			.then(keys => Promise.all(
+				keys
+					.filter(key => key !== CACHE_NAME)
+					.map(key => caches.delete(key))
+			))
+			.then(() => self.clients.claim())
+	);
+});
+
+self.addEventListener("fetch", event => {
+	if (event.request.method !== "GET") return;
+
+	event.respondWith(
+		caches.match(event.request)
+			.then(cached => cached || fetch(event.request)
+				.then(response => {
+					if (!response || !response.ok) return response;
+
+					const copy = response.clone();
+					caches.open(CACHE_NAME)
+						.then(cache => cache.put(event.request, copy));
+					return response;
+				})
+			)
+			.catch(() => {
+				if (event.request.mode === "navigate") {
+					return caches.match("./index.html");
+				}
+
+				return Response.error();
+			})
+	);
+});
